a3848ce56d78017d24021214a36bcab5fd522fc5
Shafinul Pasha <jishan523@gmail.com>
2024-10-02 20:01:16 +0600

    5-Load_More_Data The mini project is similer to the previous one (the image slider) We need to implement a button to load data which will be desabled after reaching 100 products Modified the CSS for button and product card.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Accordian from './components/accordian';
 import RandomColor from './components/2-Random_Color';
 import StarRating from './components/3-Star_Rating';
 import ImageSlider from './components/4-Image_Slider';
+import LoadMoreData from './components/5-Load_More_Data';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <ImageSlider url={"https://picsum.photos/v2/list"}
         page={"1"}
         limit={"10"}/>
+      <LoadMoreData />
     </div>
   );
 }
